252a9094e052e0133863c76ad63b8d43e09dc2b7
solarwind75 <kelvinkwok75@hotmail.com>
2022-08-12 16:57:49 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,37 +1,33 @@
-function fizzBuzz (list: number[]) {
-    for (let index = 0; index <= list.length - 1; index++) {
+function MissingPiece (list: number[]) {
+    for (let index = 0; index <= isPresent.length - 1; index++) {
         readVal = list[index]
-        if (readVal % 15 == 0) {
-            basic.showString("FizzBuzz")
-        } else {
-            if (readVal % 3 == 0) {
-                basic.showString("Fizz")
-            } else {
-                if (readVal % 5 == 0) {
-                    basic.showString("Buzz")
-                } else {
-                    basic.showNumber(readVal)
-                }
-            }
+        isPresent[readVal - 1] = 1
+    }
+    for (let index = 0; index <= 9; index++) {
+        if (isPresent[index] == 0) {
+            basic.showNumber(index + 1)
         }
     }
 }
 let readVal = 0
+let isPresent: number[] = []
 let list = [
+8,
+7,
+4,
 1,
-57,
-28,
-47,
-13,
-21,
-97,
-76,
-38,
-19,
-32,
-87,
-0,
-58,
-71
+3
 ]
-fizzBuzz(list)
+isPresent = [
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0
+]
+MissingPiece(list)
